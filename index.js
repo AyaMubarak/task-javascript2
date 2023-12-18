c11bfe09cad871849246e9e92bb14d5308ee75ce
AyaMubarak <ayamakmub@gmail.com>
2023-12-18 20:18:50 +0300

    first

---
+++ index.js
@@ -0,0 +1,5 @@
+document.querySelector("div").textContent="div";
+document.querySelector("div").classList.add("demo");
+document.querySelector("h1").textContent="h1";
+document.querySelector("h1").classList.add("vip");
+document.querySelector("h1").classList.remove("vip");

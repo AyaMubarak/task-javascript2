c237360c8680ae78c4cb69e6f17177c5c6cd9730
AyaMubarak <ayamakmub@gmail.com>
2024-01-14 23:28:30 +0300

    amount

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-document.querySelector("div").textContent="div";
-document.querySelector("div").classList.add("demo");
-document.querySelector("h1").textContent="h1";
-document.querySelector("h1").classList.add("vip");
-document.querySelector("h1").classList.remove("vip");
+var submit = document.querySelector(".registerForm");
+submit.onsubmit= function amount(e) {
+    e.preventDefault();
+    var amount = 0;
+    var dollar = "dollar";
+    var dinar = "dinar";
+    var number = document.querySelector(".amount").value;
+    var select = document.querySelector(".exchange").value;
+    var result = document.querySelector(".result");
+
+    if (select === dollar) {
+        amount = number * 3.73;
+    } else if (select === dinar) {
+        amount = number * 5.26;
+    } else {
+        amount = number;
+    }
+
+    result.textContent = amount + " nis";
+}
